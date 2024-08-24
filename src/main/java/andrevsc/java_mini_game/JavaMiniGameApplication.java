75d6f6386ab7c1b49e8b26118541f4f33212b971
AndreVsc <95365578+AndreVsc@users.noreply.github.com>
2024-08-24 03:11:43 -0300

    add project

---
+++ src/main/java/andrevsc/java_mini_game/JavaMiniGameApplication.java
@@ -0,0 +1,13 @@
+package andrevsc.java_mini_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMiniGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMiniGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andrevsc/java_mini_game/JavaMiniGameApplicationTests.java
@@ -0,0 +1,13 @@
+package andrevsc.java_mini_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMiniGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
